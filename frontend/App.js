6b836af72056dff957ffc8cc478c43baaf35ccdd
manbradcalf <ben.medcalf@gmail.com>
2023-06-28 07:42:14 -0400

    react stubs

---
+++ frontend/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Ryan won again</p>
+      </header>
+    </div>
+  );
+}
+export default App;
